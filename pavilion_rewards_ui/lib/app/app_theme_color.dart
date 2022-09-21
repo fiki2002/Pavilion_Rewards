1f1f2198e610f24bcc9de705f8dfcfd3a6624594
fiki2002 <adepitanoluwatosin202@gmail.com>
2022-09-21 10:51:46 +0100

    Added app constants

---
+++ pavilion_rewards_ui/lib/app/app_theme_color.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppThemeColor {
+  static const Color primaryColor = Color(0xff5243D2);
+  static const Color primaryTextColor = Color(0xffFFFFFF);
+  static const Color headerTextColor = Color(0xff000000);
+  static const Color minorTextColor = Color(0xffF18701);
+  static const Color secondaryColor = Color(0xff0D073C);
+  static const Color transSuccessTextColor = Color(0xffE4325A);
+  static const Color transFailureTextColor = Color(0xff1D964A);
+  static const Color transSuccessBGColor = Color(0xFF8DD7A8);
+  static const Color transFailureBGColor = Color(0xffE592A5);
+  static const Color scaffoldBGColor = Color(0xffF7F7FE);
+
+}
---
+++ pavilion_rewards_ui/lib/app/app_theme_style.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppThemeStyles {
+  static TextStyle introText = GoogleFonts.manrope(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+  );
+   static TextStyle balanceText = GoogleFonts.manrope(
+    fontSize: 9.77,
+    fontWeight: FontWeight.w700,
+  );
+   static TextStyle amountText = GoogleFonts.manrope(
+    fontSize: 7.11,
+    fontWeight: FontWeight.w500,
+  );
+   static TextStyle congratsText = GoogleFonts.manrope(
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+  );
+   static TextStyle messageText = GoogleFonts.manrope(
+    fontSize: 6,
+    fontWeight: FontWeight.w500,
+  );
+   static TextStyle redeemText = GoogleFonts.manrope(
+    fontSize: 7.34,
+    fontWeight: FontWeight.w600,
+  );
+   static TextStyle virtualText = GoogleFonts.manrope(
+    fontSize: 15.2,
+    fontWeight: FontWeight.w700,
+  );
+   static TextStyle cardDetailsText = GoogleFonts.manrope(
+    fontSize: 11.06,
+    fontWeight: FontWeight.w600,
+  );
+   static TextStyle cardDetailsTitleText = GoogleFonts.manrope(
+    fontSize: 6.91,
+    fontWeight: FontWeight.w600,
+  );
+   static TextStyle cardTypeText = GoogleFonts.manrope(
+    fontSize: 11.06,
+    fontWeight: FontWeight.w500,
+  );
+   static TextStyle dropDownText = GoogleFonts.manrope(
+    fontSize: 7.74,
+    fontWeight: FontWeight.w600,
+  );
+   static TextStyle tabText = GoogleFonts.manrope(
+    fontSize: 7.74,
+    fontWeight: FontWeight.w700,
+  );
+   static TextStyle listText = GoogleFonts.manrope(
+    fontSize: 12.38,
+    fontWeight: FontWeight.w700,
+  );
+   static TextStyle dateTimeText = GoogleFonts.manrope(
+    fontSize: 9.29,
+    fontWeight: FontWeight.w700,
+  );
+}
