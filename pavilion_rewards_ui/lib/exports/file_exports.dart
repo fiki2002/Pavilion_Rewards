52c44081132fa67ae9123543a6a5703d11f41543
fiki2002 <adepitanoluwatosin202@gmail.com>
2022-09-22 02:08:41 +0100

    Added file exports

---
+++ pavilion_rewards_ui/lib/exports/file_exports.dart
@@ -0,0 +1,23 @@
+export 'package:flutter/material.dart';
+// export 'package:device_preview/device_preview.dart';
+export 'package:pavilion_rewards_ui/app/app_theme_color.dart';
+export 'package:pavilion_rewards_ui/bottom_nav_bar.dart';
+export 'package:flutter_svg/flutter_svg.dart';
+export 'package:pavilion_rewards_ui/app/app_theme_style.dart';
+export 'package:pavilion_rewards_ui/page/home_page.dart';
+export 'package:pavilion_rewards_ui/components/redeem_container.dart';
+export 'package:pavilion_rewards_ui/components/virtual_card_list.dart';
+export 'package:pavilion_rewards_ui/components/widgets/balance_container.dart';
+export '';
+export '';
+export '';
+export '';
+export '';
+export '';
+export '';
+export '';
+export '';
+export '';
+
+
+
