bfbe680c523b6e62d98a498b2427800c753fac2c
fiki2002 <adepitanoluwatosin202@gmail.com>
2022-09-22 05:20:19 +0100

    Added trans model

--- pavilion_rewards_ui/lib/app/app_theme_color.dart
+++ pavilion_rewards_ui/lib/app/app_theme_color.dart
@@ -8,9 +8,10 @@ class AppThemeColor {
   static const Color secondaryColor = Color(0xff0D073C);
   static const Color transSuccessTextColor = Color(0xffE4325A);
   static const Color transFailureTextColor = Color(0xff1D964A);
-  static const Color transSuccessBGColor = Color(0xFF8DD7A8);
+  static const Color transSuccessBGColor = Color(0xFF8DD7AB);
   static const Color transFailureBGColor = Color(0xffE592A5);
   static const Color scaffoldBGColor = Color(0xffF7F7FE);
+  static const Color dateTimeColor = Color(0xff05040B);
   static const Color gradient1 = Color(0xff5143D2);
   static const Color gradient2 = Color(0xff491B84);
 }
--- pavilion_rewards_ui/lib/components/widgets/transaction_card.dart
+++ pavilion_rewards_ui/lib/components/widgets/transaction_card.dart
@@ -1,12 +1,64 @@
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:pavilion_rewards_ui/exports/file_exports.dart';
 
 class TransactionCard extends StatelessWidget {
-  const TransactionCard({Key? key}) : super(key: key);
-
+  TransactionCard({
+    Key? key,
+    required this.url,
+    required this.points,
+    required this.title,
+    required this.date,
+    required this.time,
+    this.isSuccess = false,
+  }) : super(key: key);
+  final String url;
+  final String title;
+  final String date;
+  final String time;
+  final String points;
+  bool isSuccess;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        color: Colors.transparent,
+      ),
+      child: Row(
+        children: [
+          CircleAvatar(
+            backgroundColor: isSuccess
+                ? AppThemeColor.transSuccessBGColor
+                : AppThemeColor.transFailureBGColor,
+            child: SvgPicture.asset(url),
+          ),
+          Text(
+            title,
+            style: AppThemeStyles.listText.copyWith(
+              color: AppThemeColor.primaryTextColor,
+            ),
+          ),
+          Text(
+            date,
+            style: AppThemeStyles.dateTimeText.copyWith(
+              color: AppThemeColor.dateTimeColor.withOpacity(0.5),
+            ),
+          ),
+          Text(
+            time,
+            style: AppThemeStyles.dateTimeText.copyWith(
+              color: AppThemeColor.dateTimeColor.withOpacity(0.5),
+            ),
+          ),
+          Text(
+            points,
+            style: AppThemeStyles.listText.copyWith(
+              color: isSuccess
+                  ? AppThemeColor.transSuccessTextColor
+                  : AppThemeColor.transFailureTextColor,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
